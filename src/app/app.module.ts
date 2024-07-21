42752d94cf5b03907db150504224a7e5b5aebc4f
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-07-21 22:18:52 +0700

    feat: create error filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 
+import { CommonModule } from '../common/common.module';
 import cookieConfig from '../config/cookie.config';
 import domainUrlConfig from '../config/domain-url.config';
 import jwtConfig from '../config/jwt.config';
+import { ErrorFilter } from '../core/filters/exception.filter';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
@@ -29,7 +32,14 @@ import { AuthModule } from './modules/auth/auth.module';
       level: 'info',
       transports: [new winston.transports.Console()],
     }),
+    CommonModule,
     AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: ErrorFilter,
+    },
   ],
 })
 export class AppModule { }
---
+++ src/core/filters/exception.filter.ts
@@ -0,0 +1,44 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { ZodError } from 'zod';
+
+import ParentCustomAPIError from '../exceptions/parent-cusom-api-error';
+import { ErrorAPIResponse } from '../models/web.model';
+
+@Catch()
+export class ErrorFilter implements ExceptionFilter {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  catch(exception: any, host: ArgumentsHost) {
+    const response = host.switchToHttp().getResponse();
+
+    if (exception instanceof ParentCustomAPIError) {
+      response
+        .status(exception.statusCode)
+        .json(
+          new ErrorAPIResponse(exception.message, exception.errors)
+        );
+      return;
+    } else if (exception instanceof ZodError) {
+      response
+        .status(HttpStatus.BAD_REQUEST)
+        .json(
+          new ErrorAPIResponse(exception.message, exception.errors)
+        );
+      return;
+    } else {
+      response
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json(
+          new ErrorAPIResponse(
+            exception?.message ?? 'Internal server error',
+            exception?.errors ?? 'Unknown error'
+          )
+        );
+      return;
+    }
+  }
+}
