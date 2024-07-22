73c0e1c73adf886a63e8ebc0b4b4cf5eeedcce99
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-07-22 09:19:30 +0700

    feat: create employee auth middleware

---
+++ src/core/middlewares/employee.auth.middleware.ts
@@ -0,0 +1,47 @@
+import { Inject, Injectable, NestMiddleware } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { NextFunction, Request, Response } from 'express';
+
+import { JwtService } from '../../common/jwt.service';
+import { PrismaService } from '../../common/prisma.service';
+import cookieConfig from '../../config/cookie.config';
+import jwtConfig from '../../config/jwt.config';
+import Unauthenticated from '../exceptions/unauthenticated';
+import { PayloadDataEmployeeAccessToken } from '../types/payload.type';
+
+@Injectable()
+export class EmployeeAuthMiddleware implements NestMiddleware {
+  constructor(
+    private jwtService: JwtService,
+    private prismaService: PrismaService,
+    @Inject(cookieConfig.KEY) private COOKIE_CONF: ConfigType<typeof cookieConfig>,
+    @Inject(jwtConfig.KEY) private JWT_CONF: ConfigType<typeof jwtConfig>,
+  ) { }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async use(req: Request & { user: any }, res: Response, next: NextFunction) {
+    const accessToken = req.cookies[this.COOKIE_CONF.employeeAccessTokenCookieName];
+    if (!accessToken) {
+      throw new Unauthenticated('Unauthenticated user. Please login.', 'Unauthenticated user.');
+    }
+
+    const payload = this.jwtService.checkIsValid<PayloadDataEmployeeAccessToken>(
+      accessToken,
+      this.JWT_CONF.employeeJwtAccessTokenSecret,
+    );
+
+    const dbEmployee = await this.prismaService.employee.findUnique({
+      where: {
+        id: payload.id,
+      },
+    });
+
+    if (!dbEmployee) {
+      throw new Unauthenticated('Unauthenticated user. Please login.', 'Unauthenticated user.');
+    }
+
+    req.user = dbEmployee;
+
+    next();
+  }
+}
