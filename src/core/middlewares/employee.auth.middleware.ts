0aae92f99465f1a8b3e6dde9a22c9a790282df50
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-08-21 21:51:27 +0700

    chore: implement typeorm on employee auth middleware

--- src/core/middlewares/employee.auth.middleware.ts
+++ src/core/middlewares/employee.auth.middleware.ts
@@ -1,9 +1,11 @@
 import { Inject, Injectable, NestMiddleware } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
 import { NextFunction, Request, Response } from 'express';
+import { Repository } from 'typeorm';
 
+import { Employee } from '../../../database/entities/employee.entity';
 import { JwtService } from '../../common/jwt.service';
-import { PrismaService } from '../../common/prisma.service';
 import cookieConfig from '../../config/cookie.config';
 import jwtConfig from '../../config/jwt.config';
 import Unauthenticated from '../exceptions/unauthenticated';
@@ -12,8 +14,9 @@ import { PayloadDataEmployeeAccessToken } from '../types/payload.type';
 @Injectable()
 export class EmployeeAuthMiddleware implements NestMiddleware {
   constructor(
+    @InjectRepository(Employee)
+    private employeeRepository: Repository<Employee>,
     private jwtService: JwtService,
-    private prismaService: PrismaService,
     @Inject(cookieConfig.KEY) private COOKIE_CONF: ConfigType<typeof cookieConfig>,
     @Inject(jwtConfig.KEY) private JWT_CONF: ConfigType<typeof jwtConfig>,
   ) { }
@@ -23,29 +26,39 @@ export class EmployeeAuthMiddleware implements NestMiddleware {
     const { cookies } = req;
 
     if (!cookies) {
-      throw new Unauthenticated('Unauthenticated user. Please login.', 'Unauthenticated user.');
+      throw new Unauthenticated(
+        'Unauthenticated user. Please login.',
+        'Unauthenticated user.',
+      );
     }
 
     const cookieName = this.COOKIE_CONF.employeeAccessTokenCookieName;
     const accessToken = cookies[cookieName];
 
     if (!accessToken) {
-      throw new Unauthenticated('Unauthenticated user. Please login.', 'Unauthenticated user.');
+      throw new Unauthenticated(
+        'Unauthenticated user. Please login.',
+        'Unauthenticated user.',
+      );
     }
 
-    const payload = this.jwtService.checkIsValid<PayloadDataEmployeeAccessToken>(
-      accessToken,
-      this.JWT_CONF.employeeJwtAccessTokenSecret,
-    );
+    const payload =
+      this.jwtService.checkIsValid<PayloadDataEmployeeAccessToken>(
+        accessToken,
+        this.JWT_CONF.employeeJwtAccessTokenSecret,
+      );
 
-    const dbEmployee = await this.prismaService.employee.findUnique({
+    const dbEmployee = await this.employeeRepository.findOne({
       where: {
         id: payload.id,
       },
     });
 
     if (!dbEmployee) {
-      throw new Unauthenticated('Unauthenticated user. Please login.', 'Unauthenticated user.');
+      throw new Unauthenticated(
+        'Unauthenticated user. Please login.',
+        'Unauthenticated user.',
+      );
     }
 
     req.user = dbEmployee;
